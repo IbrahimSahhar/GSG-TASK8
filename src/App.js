7a48e279b606d98270177d8b55dad4a99e4efb3e
Ibrahim <113946679+IbrahimSahhar@users.noreply.github.com>
2022-12-20 12:43:59 +0300

    final edit in validation

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
-import './App.css';
-import Counter from './counter';
-import Logo from './logo';
+import "./App.css";
+import Counter from "./counter";
+import Logo from "./logo";
 
-
-
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class App extends Component {
-  state={
-    total :0
-    
-  }
-  sumTotal =(countValue)=>{
-    this.setState((prevState)=>{
-    if(prevState.total>= 0) return {total : prevState.total +countValue} ;
-    })
-  }
+  state = {
+    total: 0,
+  };
+  sumTotal = (countValue) => {
+    this.setState((prevState) => {
+      if (prevState.total >= 0) return { total: prevState.total + countValue };
+    });
+  };
   render() {
     return (
       <div>
-        < Logo />
-        <Counter getTotal ={this.sumTotal} numberOfCount ='1'  />
-        <Counter getTotal ={this.sumTotal}  numberOfCount ='2'/>
-        <Counter getTotal ={this.sumTotal}  numberOfCount ='3'/>
-        <Counter getTotal ={this.sumTotal}  numberOfCount ='4'/>
-        
+        <Logo />
+        <Counter getTotal={this.sumTotal} numberOfCount="1" />
+        <Counter getTotal={this.sumTotal} numberOfCount="2" />
+        <Counter getTotal={this.sumTotal} numberOfCount="3" />
+        <Counter getTotal={this.sumTotal} numberOfCount="4" />
 
-
-        <div className='totalValues'>
-          {this.state.total}
-        </div>
-        
+        <div className="totalValues">{this.state.total}</div>
       </div>
-    )
+    );
   }
 }
-
-
 
 export default App;
--- src/counter.jsx
+++ src/counter.jsx
@@ -9,14 +9,12 @@ export default class Counter extends Component {
     this.setState((prev) => ({
       count: prev.count + +this.props.numberOfCount,
     }));
-    if (this.state.count >= 0) {
-      this.props.getTotal(+this.props.numberOfCount);
-    }
+    this.props.getTotal(+this.props.numberOfCount);
   };
 
   decrement = () => {
     this.setState((prevState) => {
-      if (prevState.count > 0) {
+      if (prevState.count >= +this.props.numberOfCount) {
         return { count: prevState.count - +this.props.numberOfCount };
       }
     });
@@ -39,48 +37,3 @@ export default class Counter extends Component {
     );
   }
 }
-
-// import React, { Component } from "react";
-
-// export default class Counter extends Component {
-//   counter = `count${this.props.numberOfCounter}`;
-//   state = {
-//     counter: 0,
-//   };
-
-//   increment = () => {
-//     this.setState({
-//       counter: 0,
-//     });
-
-//     this.setState((prev) => ({
-//       counter: prev.counter + 1,
-//     }));
-//     if (this.state.counter >= 0) {
-//       this.props.getTotal(1);
-//     }
-//   };
-
-//   decrement = () => {
-//     this.setState((prevState) => {
-//       if (prevState.counter > 0) return { counter: prevState.counter - 1 };
-//     });
-//     if (this.state.counter >= 0) {
-//       this.props.getTotal(-1);
-//     }
-//   };
-
-//   render() {
-//     return (
-//       <div className="counter">
-//         <div className="count">{this.state.counter}</div>
-//         <div className="increment" onClick={(e) => this.increment()}>
-//           +
-//         </div>
-//         <div className="decrement" onClick={(e) => this.decrement()}>
-//           -
-//         </div>
-//       </div>
-//     );
-//   }
-// }
